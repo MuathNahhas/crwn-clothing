638843f08fd531e31ac9ed48b676ba8ff384a4c5
Muath <muath.nahhas@yahoo.com>
2024-06-29 13:00:04 +0300

    create button component and continue work in firebase

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -2,15 +2,19 @@ import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
+
+import SignUpForm from "../../component/sign-up-form/sign-up-form-component";
 const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
+    console.log("userDocRef", userDocRef);
   };
   return (
     <div>
       <h1>SignIn page</h1>
       <button onClick={logGoogleUser}>sign in with google popup</button>
+      <SignUpForm />
     </div>
   );
 };
