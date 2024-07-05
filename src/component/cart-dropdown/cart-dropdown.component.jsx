ef47c2ab04669a198483302624725e13ee927e12
Muath <muath.nahhas@yahoo.com>
2024-07-05 14:07:41 +0300

    create checkout-item component and integrate with data

--- src/component/cart-dropdown/cart-dropdown.component.jsx
+++ src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,15 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
 import "./cart-dropdown.styles.scss";
 import { CartContext } from "../../contexts/cart.context";
 const CartDropDown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+  const goToChecOutHandler = () => {
+    navigate("checkout");
+  };
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -12,7 +17,7 @@ const CartDropDown = () => {
           return <CartItem key={item.id} cartItem={item} />;
         })}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToChecOutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 };
